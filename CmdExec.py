20e7faa9c6aa3a06fb8616ca513bd78183567ea3
Bradford Holden <brad.p.holden@gmail.com>
2020-02-11 08:42:00 -0800

    helper module

---
+++ CmdExec.py
@@ -0,0 +1,28 @@
+import subprocess
+
+import apflog
+
+def cmdexec(cmd, cwd='./',fake=False,debug=False):
+    args = ["apftask","do"]
+    args = args + cmd.split()
+    if fake:
+        apflog("Would have executed: %s" % repr(cmd), echo=True)
+        return True, 0
+    
+    apflog("Executing Command: %s" % repr(cmd), echo=True)
+    
+    p = subprocess.Popen(args, stdout=subprocess.PIPE,stderr=subprocess.PIPE,cwd=cwd)
+    
+    while p.poll() is None:
+        l = p.stdout.readline().rstrip('\n')
+        if debug: apflog(l, echo=debug)
+
+    out, err = p.communicate()
+    if debug: apflog(out, echo=debug)
+    if len(err): apflog(err, echo=debug)
+    ret_code = p.returncode
+    if ret_code == 0:
+        return True, ret_code
+    else:
+        return False, ret_code
+
