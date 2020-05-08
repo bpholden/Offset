8e80e0dfbd4f992cfbf2f7056076f8d354a405b1
Bradford Holden <brad.p.holden@gmail.com>
2020-05-07 18:23:51 -0700

    bump up total time

--- Star.py
+++ Star.py
@@ -174,7 +174,7 @@ class Star:
 
 
             
-        self.tottime = (self.texp) * self.count  + 100 
+        self.tottime = (self.texp) * self.count  + 300 
         if self.count > 1:
             self.tottime += 40*(self.count - 1)
         if self.blank is True:
